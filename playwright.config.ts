2eb3d3cfbfe859ac86a81c9a730ea192a5e403e2
Sayali Dange <sayali.dange@turtlemint.com>
2025-07-29 14:16:57 +0530

    Update Playwright config and shell script for Jenkins

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const isCI = !!process.env.CI; // true if running in CI like Jenkins
+
 export default defineConfig({
   testDir: './tests',
   timeout: 30000,
@@ -10,27 +12,25 @@ export default defineConfig({
     timeout: 5000,
   },
   retries: 0,
-  // 👇 Add project with slowMo
   projects: [
     {
       name: 'chromium',
       use: {
         browserName: 'chromium',
         baseURL: process.env.BASE_URL,
-        headless: false,
+        headless: isCI, 
         screenshot: 'only-on-failure',
         video: 'retain-on-failure',
         trace: 'retain-on-failure',
-        // ✅ slowMo goes here
         launchOptions: {
-          slowMo: 500,
+          slowMo: isCI ? 0 : 500, // disable slowMo in CI
         },
       },
     },
   ],
   reporter: [
     ['list'],
-    ['html' , { outputFolder: 'playwright-report' }],
+    ['html', { outputFolder: 'playwright-report' }],
     ['allure-playwright'],
   ],
 });
