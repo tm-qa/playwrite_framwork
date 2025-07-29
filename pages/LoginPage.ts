68887c8064f60100cef767cf60b71a602348fc6c
Sayali Dange <sayali.dange@turtlemint.com>
2025-07-29 14:41:11 +0530

    Update Playwright config and shell script for Jenkins

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -11,43 +11,47 @@ export class NinjaPage {
   }
 
   async loginToGmailAndNinja(email: string, password: string) {
-  const ninjaUrl = process.env.NINJA_URL || 'https://ninja.sanity.turtle-feature.com/';
-  await this.page.goto(ninjaUrl);
-  await this.page.waitForLoadState('networkidle');
-  console.log('Navigated to Ninja login page');
+   
+    const ninjaUrl = process.env.NINJA_URL || 'https://ninja.sanity.turtle-feature.com/';
+    await this.page.goto(ninjaUrl);
+    await this.page.waitForLoadState('networkidle');
+    console.log('Navigated to Ninja login page');
+    const screenshot = await this.page.screenshot();
+    attachment('Login Page Screenshot', screenshot, 'image/png');
 
- 
-  const screenshot = await this.page.screenshot();
-  attachment('Login Page Screenshot', screenshot, 'image/png');
-
-  
-  console.log('Clicking on Google Sign-In...');
-  const [popup] = await Promise.all([
-    this.page.waitForEvent('popup'),
-    this.page.locator('//span/a[@id="google-signin-button"]').click(),
+   const [popup] = await Promise.all([
+    this.page.context().waitForEvent('page'), 
+    this.page.locator('//span/a[@id="google-signin-button"]').click() 
   ]);
 
   await popup.waitForLoadState('load');
   console.log('Google login popup opened');
 
-  
-  await popup.locator('#identifierId').fill(email);
-  await popup.locator('#identifierNext').click();
 
-  
+  await popup.locator('#identifierId').fill(email);
+  debugger;
+  await popup.locator('#identifierNext').click();
   await popup.waitForSelector('input[name="Passwd"]', { timeout: 10000 });
   await popup.locator('input[name="Passwd"]').fill(password);
   await popup.locator('#passwordNext').click();
 
-  console.log('Email and password submitted');
+  console.log(' Email and password submitted');
 
-  
-  await this.page.waitForLoadState('networkidle', { timeout: 20000 });
-
-  
-  const loggedInElement = this.page.locator('a[data-auto="qis-module"]');
-  await expect(loggedInElement).toBeVisible({ timeout: 15000 });
-  console.log('Login successful, Quote Request module visible');
+  await this.page.waitForTimeout(10000);
 }
+
+async clickOnModule (module : string ){
+  await this.page.waitForTimeout(3000);
+  const screenshot1 = await this.page.screenshot();
+  attachment('Login Page Screenshot', screenshot1, 'image/png');
+  const moduleLocator = this.page.locator('a[data-auto="qis-module"]');
+  await moduleLocator.waitFor({ state: 'visible', timeout: 5000 });
+  await moduleLocator.click();
+  console.log(' Clicked on Quote Request module');
+
+  attachment('Login Page Screenshot', screenshot1, 'image/png');
+
+}
+
 
 }
