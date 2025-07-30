f887f68105187fc3774409366db0da0153ccb3fe
Sayali Dange <sayali.dange@turtlemint.com>
2025-07-30 16:12:42 +0530

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,8 +32,8 @@ export default defineConfig({
       use: {
         browserName: 'firefox',
         baseURL: process.env.BASE_URL,
-        headless: true,
-        // headless: isCI,
+        // headless: true,
+      headless: isCI,
         screenshot: 'only-on-failure',
         video: 'retain-on-failure',
         trace: 'retain-on-failure',
